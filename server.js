5bcfcc633a88f41ec1dec57b46e67bc07055662b
Siwakorn Banluesapy <siwakorn167@gmail.com>
2023-03-05 10:07:10 +0700

    new change

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 9000;
+const cors = require("cors");
+app.use(express.json(), cors());
 const fullname = "Team Project";
 //  data
 const members = [
